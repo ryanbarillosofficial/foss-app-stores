23d5b864c4b55570756224051c43c3c1ea2b341e
ryanbarillos <129530399+ryanbarillos@users.noreply.github.com>
2024-06-30 16:09:55 -0700

    Initial commit

---
+++ customize.sh
@@ -0,0 +1,21 @@
+SKIPMOUNT=false
+PROPFILE=false
+POSTFSDATA=false
+LATESTARTSERVICE=false
+REPLACE_EXAMPLE=""
+REPLACE=""
+
+print_modname() {
+  ui_print "*************************"
+  ui_print "FOSS App Stores Installer"
+  ui_print "*************************"
+}
+
+on_install() {
+  ui_print "Extracting module files"
+  unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
+}
+
+set_permissions() {
+  set_perm_recursive $MODPATH 0 0 0755 0644
+}
